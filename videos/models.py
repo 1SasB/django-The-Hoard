6839a01601166b285da0f02cba214f8b4071185a
Benjamin Yeboah Sasu <sasuyeboahbenjamin@gmail.com>
2021-12-18 14:33:05 +0000

    added subscribe models

--- videos/models.py
+++ videos/models.py
@@ -17,17 +17,28 @@ visibilty = (
 )
 
 
-
-
 class Hord(models.Model):
     Name = models.CharField(max_length=200)
     description = models.CharField(max_length=1000)
     profile_image = models.ImageField(upload_to='images/profile/')
     wall_paper = models.ImageField(upload_to='images/wall/')
+    subscribers  = models.ManyToManyField('auth.User', through='Subscribers',related_name='hoard_subscribers')
     owner = models.OneToOneField('auth.user',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.Name
+
+class Subscribers(models.Model):
+    hord = models.ForeignKey(Hord,on_delete=models.CASCADE)
+    user = models.ForeignKey('auth.User',on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('hord','user')
+
+    def __str__(self):
+        return '%s subscribed to %s'%(self.user.username,self.hord.Name)
+
+
     
 
 class SubHord(models.Model):
